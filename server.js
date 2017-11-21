26625f0b68618ab4cfbf353cac6bb707d272caf6
Artem Drashpul <adrashpul@dataart.com>
2017-11-21 22:44:24 +0100

    Added projects page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        title: 'Projects Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Page does not exist',
